39420e30248cc2452166dc3de9001babf0a0c865
Restuccia Omar Gabriel [huxu4797] <omargabriel.restuccia@lisholding.it>
2025-01-08 16:40:15 +0100

    aggiunti tutti i moduli (23 design della GoF + MVC + MVVM)

--- Singleton/src/main/java/com/crustwerk/Main.java
+++ Singleton/src/main/java/com/crustwerk/Main.java
@@ -1,5 +1,14 @@
 package com.crustwerk;
 
+/*
+Necessità:
+    Garantire che una classe abbia una sola istanza e fornire un punto di accesso globale a questa istanza.
+
+Quando usarlo:
+    Quando è necessario un oggetto centrale che venga utilizzato da tutto il sistema,
+    come una connessione al database o una gestione delle configurazioni.
+*/
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
--- Template/src/main/java/com/crustwerk/Main.java
+++ Template/src/main/java/com/crustwerk/Main.java
@@ -1,5 +1,14 @@
 package com.crustwerk;
 
+/*
+Necessità:
+    Definire l'ossatura di un algoritmo in un metodo, lasciando alcuni passi a specifiche implementazioni delle sottoclassi.
+
+Quando usarlo:
+    Quando hai una sequenza di operazioni comuni che devono essere implementate in modo simile da classi diverse,
+    ma con alcuni passaggi personalizzati.
+*/
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
