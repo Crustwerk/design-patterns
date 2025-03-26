8fdc3625c290ff45842ec013deb76075770ef98b
Crustwerk <omar.restuccia@live.it>
2025-03-26 15:43:58 +0100

    aggiunte note su Factory

--- factory/src/main/java/com/crustwerk/Main.java
+++ factory/src/main/java/com/crustwerk/Main.java
@@ -2,14 +2,26 @@ package com.crustwerk;
 
 /*
 Necessità:
-Definire un'interfaccia per creare oggetti, delegando l'implementazione alle sottoclassi.
+    - Definire un'interfaccia per creare oggetti, delegando l'implementazione alle sottoclassi.
 
-Quando usarlo:
-Quando non vuoi legare il codice a una classe concreta.
+Vantaggi:
+    - Disaccoppiamento: il codice che usa l'oggetto non dipende dalla sua implementazione concreta.
+        Il codice non dipende più direttamente dalle classi specifiche (Tesla, Toyota), ma solo dall'interfaccia comune (Car) e dalla factory.
+        Questo rende il codice più flessibile e facile da mantenere.
+
+    - Facile estensione: possiamo modificare la creazione dell'oggetto senza cambiare il codice che lo utilizza.
+
+    - Migliore manutenibilità: se la logica di creazione cambia, dobbiamo modificarla solo nella factory.
 */
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Creiamo una fabbrica di motociclette
+        MotorVehicleFactory motorcycleFactory = new MotorcycleFactory();
+        MotorVehicle motorcycle = motorcycleFactory.create(); // Output: Build Motorcycle
+
+        // Creiamo una fabbrica di auto
+        MotorVehicleFactory carFactory = new CarFactory();
+        MotorVehicle car = carFactory.create(); // Output: Build Car
     }
 }
